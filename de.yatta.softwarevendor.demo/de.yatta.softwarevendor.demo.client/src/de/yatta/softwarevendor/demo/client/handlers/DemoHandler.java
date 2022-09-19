6dd98f65f339d141be4a0091c38cbc69677eccdb
Florian Heerdegen <florian.heerdegen@yatta.de>
2022-09-19 21:15:14 +0200

    MP-3772: Cleanup DemoHandler, omit license check if not signed in
    - remove unnecessary email access
    - add info dialog for validity 'WAIT'
    - do not ask for licenses if user is not signed in
    - allow for one time purchase of demo game
    - format source code of DemoHandler
    - invert equals so that String constants come first
    - use '==' instead of equals for enum constants
    - add Thread.sleep in fetchLicenseStatus

--- de.yatta.softwarevendor.demo/de.yatta.softwarevendor.demo.client/src/de/yatta/softwarevendor/demo/client/handlers/DemoHandler.java
+++ de.yatta.softwarevendor.demo/de.yatta.softwarevendor.demo.client/src/de/yatta/softwarevendor/demo/client/handlers/DemoHandler.java
@@ -27,112 +27,128 @@ import de.yatta.platform.marketplace.licensing.client.LicensingClient;
 
 public class DemoHandler extends AbstractHandler implements EventHandler {
 
-	private static final String VENDOR_KEY = "g5JE78Z0UIiQrHCAMjTR";
-	private static final String SOLUTION_ID = "de.softwarevendor.product";
+  private static final String SOLUTION_ID = "de.softwarevendor.product";
+  private static final String SOLUTION_ID_ONETIMEPURCHASE = "de.softwarevendor.product.onetimepurchase";
+  private static final String VENDOR_KEY = "g5JE78Z0UIiQrHCAMjTR";
 
-	private int checkoutDuration = 1;
-	private Game game = Game.CLUMSY_BIRD;
-	private IWorkbenchWindow window = null;
-	private boolean openedGame = false;
+  private int checkoutDuration = 1; // Time in minutes how long the checkout token is valid
+  private Game game = Game.CLUMSY_BIRD;
+  private IWorkbenchWindow window = null;
+  private boolean openedGame = false;
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MarketplaceClientPlugin plugin = MarketplaceClientPlugin.getDefault();
-		plugin.registerEventHandler(this, MarketplaceClient.ACCOUNT_LOGGED_IN_EVENT);
-		plugin.registerEventHandler(this, MarketplaceClient.ACCOUNT_LOGGED_OUT_EVENT);
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+    MarketplaceClientPlugin plugin = MarketplaceClientPlugin.getDefault();
+    plugin.registerEventHandler(this, MarketplaceClient.ACCOUNT_LOGGED_IN_EVENT);
+    plugin.registerEventHandler(this, MarketplaceClient.ACCOUNT_LOGGED_OUT_EVENT);
 
-		IPartListener2 pl = new IPartListener2() {
-			public void partClosed(IWorkbenchPartReference partRef) {
-				if (partRef.getTitle().equals("Commercial Checkout"))
-					openedGame = false;
-			}
+    IPartListener2 pl = new IPartListener2() {
+      public void partClosed(IWorkbenchPartReference partRef) {
+        if (partRef.getTitle().equals("Commercial Checkout"))
+          openedGame = false;
+      }
 
-		};
-		window.getActivePage().addPartListener(pl);
+    };
+    window.getActivePage().addPartListener(pl);
 
-		if (!AccountManager.get().isSignedIn()) {
-			MessageDialog.openInformation(window.getShell(), game.toString(),
-					"Hi,\nto play " + game.toString() + " you need to sign in or subscribe for a license.");
-			closeCheckoutTab();
-			MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), SOLUTION_ID);
-			openedGame = true;
-		}
-		String emailAdress = AccountManager.get().getEmail().get();
-		final LicenseResponse fetchLicense = fetchLicenseStatus(2000);
+    if (!AccountManager.get().isSignedIn()) {
+      MessageDialog.openInformation(window.getShell(), game.toString(),
+          "Hi,\nto play " + game.toString() + " you need to sign in or subscribe for a license.");
+      closeCheckoutTab();
+      MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), SOLUTION_ID);
+      openedGame = true;
+      return null;
+    }
+    final LicenseResponse fetchLicense = fetchLicenseStatus(2000);
 
-		if (fetchLicense.getValidity().equals(Validity.UNLICENSED)) {
-			MessageDialog.openInformation(window.getShell(), game.toString(), "Hi " + emailAdress
-					+ ",\nYou dont have a license to play " + game.toString() + ". Please subscribe to play.");
-			closeCheckoutTab();
-			MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), SOLUTION_ID);
-		} else if (fetchLicense.getValidity().equals(Validity.LICENSED)) {
-			openBrowserUrl(game.getUrl());
-		}
+    if (fetchLicense.getValidity() == Validity.UNLICENSED) {
+      String[] buttons = { "Purchase", "Subscribe", "Cancel" };
+      MessageDialog dialog = new MessageDialog(window.getShell(), game.toString(), null,
+          "We couldn't detect a valid license, please go ahead and purchase or subscribe for a license.",
+          MessageDialog.INFORMATION, buttons, 2);
+      int buttonIndex = dialog.open();
 
-		return null;
-	}
+      if (buttonIndex == 0) {
+        closeCheckoutTab();
+        MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), SOLUTION_ID_ONETIMEPURCHASE);
+      } else if (buttonIndex == 1) {
+        closeCheckoutTab();
+        MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), SOLUTION_ID);
+      }
+    } else if (fetchLicense.getValidity() == Validity.WAIT) {
+      MessageDialog.openInformation(window.getShell(), game.toString(),
+          "There was an error communicating with the licensing server.");
+    } else if (fetchLicense.getValidity() == Validity.LICENSED) {
+      openBrowserUrl(game.getUrl());
+    }
 
-	private LicenseResponse fetchLicense(String solutionId) {
-		return LicensingClient.get().queryLicense(new LicenseRequest(solutionId, null, checkoutDuration, VENDOR_KEY));
-	}
+    return null;
+  }
 
-	private void openBrowserUrl(String url) {
-		closeCheckoutTab();
-		MarketplaceClientPlugin.getDefault().getSolutionIdToRequest().put("de.softwarevendor.demo.url", url);
-		MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), null);
-		MarketplaceClientPlugin.getDefault().getSolutionIdToRequest().put("de.softwarevendor.demo.url", null);
-		openedGame = true;
-	}
+  private LicenseResponse fetchLicense(String solutionId) {
+    return LicensingClient.get().queryLicense(new LicenseRequest(solutionId, null, checkoutDuration, VENDOR_KEY));
+  }
 
-	@Override
-	public void handleEvent(Event event) {
+  private void openBrowserUrl(String url) {
+    closeCheckoutTab();
+    MarketplaceClientPlugin.getDefault().getSolutionIdToRequest().put("de.softwarevendor.demo.url", url);
+    MarketplaceClient.get().openCheckout(MarketplaceClientPlugin.getDisplay(), null);
+    MarketplaceClientPlugin.getDefault().getSolutionIdToRequest().put("de.softwarevendor.demo.url", null);
+    openedGame = true;
+  }
 
-		if (event.getTopic().equals(MarketplaceClient.ACCOUNT_LOGGED_IN_EVENT)) {
-			LicenseResponse fetchLicense = fetchLicenseStatus(1000);
+  @Override
+  public void handleEvent(Event event) {
 
-			System.out.println(AccountManager.get().getEmail().get());
+    if (MarketplaceClient.ACCOUNT_LOGGED_IN_EVENT.equals(event.getTopic())) {
+      LicenseResponse licenseResponse = fetchLicenseStatus(1000);
 
-			if (fetchLicense.getValidity().equals(Validity.LICENSED) && findCheckoutTab().isPresent() && !openedGame) {
-				Display.getDefault().syncExec(() -> {
-					MessageDialog.openInformation(window.getShell(), game.toString(),
-							"Successfully subscribed! Lets start " + game.toString());
-					openBrowserUrl(game.getUrl());
-					String text = window.getWorkbench().getDisplay().getActiveShell().getText();
-					System.out.println(text);
-				});
-			}
-			return;
-		}
+      if (licenseResponse.getValidity() == Validity.LICENSED && findCheckoutTab().isPresent() && !openedGame) {
+        Display.getDefault().syncExec(() -> {
+          MessageDialog.openInformation(window.getShell(), game.toString(),
+              "Successfully subscribed! Lets start " + game.toString());
+          openBrowserUrl(game.getUrl());
+          String text = window.getWorkbench().getDisplay().getActiveShell().getText();
+          System.out.println(text);
+        });
+      }
+      return;
+    }
 
-		if (event.getTopic().equals(MarketplaceClient.ACCOUNT_LOGGED_OUT_EVENT)) {
-			openedGame = false;
-			closeCheckoutTab();
-			return;
-		}
-	}
+    if (MarketplaceClient.ACCOUNT_LOGGED_OUT_EVENT.equals(event.getTopic())) {
+      openedGame = false;
+      closeCheckoutTab();
+      return;
+    }
+  }
 
-	private LicenseResponse fetchLicenseStatus(final long timeoutInMillis) {
-		LicenseResponse licenseResponse;
-		final long tryUntil = timeoutInMillis + System.currentTimeMillis();
-		do {
-			licenseResponse = fetchLicense(SOLUTION_ID);
-		} while (licenseResponse.getValidity().equals(Validity.WAIT) && System.currentTimeMillis() <= tryUntil);
-		return licenseResponse;
-	}
+  private LicenseResponse fetchLicenseStatus(final long timeoutInMillis) {
+    LicenseResponse licenseResponse = fetchLicense(SOLUTION_ID);
+    final long tryUntil = System.currentTimeMillis() + timeoutInMillis;
+    while (licenseResponse.getValidity() == Validity.WAIT && System.currentTimeMillis() <= tryUntil) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      licenseResponse = fetchLicense(SOLUTION_ID);
+    }
+    return licenseResponse;
+  }
 
-	private void closeCheckoutTab() {
-		findCheckoutTab().ifPresent(checkoutTab -> {
-			Display.getDefault().syncExec(() -> {
-				window.getActivePage().closeEditor(checkoutTab.getEditor(false), false);
-			});
-			openedGame = false;
-		});
-	}
+  private void closeCheckoutTab() {
+    findCheckoutTab().ifPresent(checkoutTab -> {
+      Display.getDefault().syncExec(() -> {
+        window.getActivePage().closeEditor(checkoutTab.getEditor(false), false);
+      });
+      openedGame = false;
+    });
+  }
 
-	private Optional<IEditorReference> findCheckoutTab() {
-		return Arrays.stream(window.getActivePage().getEditorReferences())
-				.filter(editor -> "Commercial Checkout".equals(editor.getTitle())).findFirst();
-	}
+  private Optional<IEditorReference> findCheckoutTab() {
+    return Arrays.stream(window.getActivePage().getEditorReferences())
+        .filter(editor -> "Commercial Checkout".equals(editor.getTitle())).findFirst();
+  }
 
 }
