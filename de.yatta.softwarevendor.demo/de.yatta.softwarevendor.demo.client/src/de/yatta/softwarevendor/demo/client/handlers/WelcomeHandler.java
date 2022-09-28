350d72d60e511cc0093974a915d914e1a8f5a8f7
Florian Heerdegen <florian.heerdegen@yatta.de>
2022-09-28 23:07:05 +0200

    MP-3771: Add checkbox to welcome page

--- de.yatta.softwarevendor.demo/de.yatta.softwarevendor.demo.client/src/de/yatta/softwarevendor/demo/client/handlers/WelcomeHandler.java
+++ de.yatta.softwarevendor.demo/de.yatta.softwarevendor.demo.client/src/de/yatta/softwarevendor/demo/client/handlers/WelcomeHandler.java
@@ -11,13 +11,21 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.BrowserFunction;
+import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
 
+import de.yatta.softwarevendor.demo.VendorDemoPlugin;
 import de.yatta.softwarevendor.demo.client.ui.BrowserWrapper;
 import de.yatta.softwarevendor.demo.client.ui.BrowserWrapperInput;
+import de.yatta.softwarevendor.demo.client.ui.SoftwareVendorPreferencePage;
 
 public class WelcomeHandler extends AbstractHandler {
 
@@ -39,7 +47,34 @@ public class WelcomeHandler extends AbstractHandler {
             URL url = new URL(base, "index.html");
             IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
             IWorkbenchPage page = window.getActivePage();
-            page.openEditor(new BrowserWrapperInput(BROWSER_TITLE, url), BrowserWrapper.EDITOR_ID);
+            BrowserWrapper browserWrapper = (BrowserWrapper) page
+                .openEditor(new BrowserWrapperInput(BROWSER_TITLE, url), BrowserWrapper.EDITOR_ID);
+            Browser browser = browserWrapper.getBrowser();
+
+            // activate checkbox if setting is active
+            final Preferences preferences = InstanceScope.INSTANCE.getNode(VendorDemoPlugin.PLUGIN_ID);
+            if (preferences.getBoolean(SoftwareVendorPreferencePage.DO_NOT_SHOW_WELCOME_PAGE, false)) {
+              String script = "document.getElementById('" + SoftwareVendorPreferencePage.DO_NOT_SHOW_WELCOME_PAGE
+                  + "').checked=true";
+              browser.addProgressListener(ProgressListener.completedAdapter(evt -> browser.execute(script)));
+            }
+
+            // toggle setting when checkbox is checked/unchecked
+            new BrowserFunction(browser, "doNotShowWelcomePageChecked") {
+              @Override
+              public Object function(Object[] arguments) {
+                if (arguments != null && arguments.length > 0) {
+                  boolean value = Boolean.TRUE.equals(arguments[0]);
+                  preferences.putBoolean(SoftwareVendorPreferencePage.DO_NOT_SHOW_WELCOME_PAGE, value);
+                  try {
+                    preferences.flush();
+                  } catch (BackingStoreException e) {
+                    throw new RuntimeException(e);
+                  }
+                }
+                return true;
+              }
+            };
           } catch (PartInitException | IOException e) {
             throw new RuntimeException(e);
           }
