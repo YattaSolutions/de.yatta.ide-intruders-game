75c0cff983f5a6b9d63ac3c261c0ea6b35d4d0b5
Leif Geiger <leif.geiger@yatta.de>
2023-04-14 17:39:40 +0200

    MP-5253: adapt to new API

--- de.yatta.ideintruders.client/src/de/yatta/ideintruders/VendorDemoPlugin.java
+++ de.yatta.ideintruders.client/src/de/yatta/ideintruders/VendorDemoPlugin.java
@@ -3,10 +3,13 @@ package de.yatta.ideintruders;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import com.yattasolutions.platform.marketplace.client.CheckoutEnvironment;
+
 public class VendorDemoPlugin extends AbstractUIPlugin {
 
-  public static final String SOLUTION_ID = "preview.de.softwarevendor.product";
-  public static final String SOLUTION_ID_ONETIMEPURCHASE = "preview.de.softwarevendor.product.onetimepurchase";
+  public static final CheckoutEnvironment ENVIRONMENT = CheckoutEnvironment.LIVE;
+  public static final String PRODUCT_ID = "de.softwarevendor.product";
+  public static final String PRODUCT_ID_ONETIMEPURCHASE = "de.softwarevendor.product.onetimepurchase";
   public static final String VENDOR_KEY = "DEMO";
 
   public static final String PLUGIN_ID = "de.yatta.ideintruders.client";
